7ae69c3b8b07d2a3eb89a0c0ff8ae6510ac051fa
wendymarconi <wendymarconiquintero28@gmail.com>
2022-03-16 20:20:17 -0500

    Archivos iniciales

---
+++ main.js
@@ -0,0 +1,5 @@
+addEventListener("DOMContentLoaded", async(e)=>{
+    let peticion  = await fetch("https://wendymarconi.000webhostapp.com/Variables_PHP_21_02_2022/api.php");
+    let texto = await peticion.text();
+    document.body.insertAdjacentHTML("afterend",texto);
+})
